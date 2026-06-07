8e7cdef2aebd8267ac116ea02fd48184780a68a7
Lok Lok <yellowhappy831@gmail.com>
2026-06-07 18:39:23 +0800

    sleepy lamb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,11 @@ import { MoreContent } from "./_ui/MoreContent";
 import { VideoBackground } from "./_ui/VideoBackground";
 
 export default function Page() {
+  const height = useHeight();
+
   return (
     <>
-      <div className=" absolute top-0 left-0 w-full h-full select-none">
+      <div className=" fixed top-0 left-0 w-full h-full select-none">
         <VideoBackground
           adsID={"#ads"}
           containerID={"#my-container"}
@@ -21,15 +23,17 @@ export default function Page() {
       >
         <Padding></Padding>
         <div id="ads">
-          <HeroContent></HeroContent>
-          <MoreContent></MoreContent>
+          <div
+            className="w-full bg-gray-200"
+            style={{ height: `${height}` }}
+          ></div>
         </div>
       </div>
     </>
   );
 }
 
-function Padding({}) {
+const useHeight = () => {
   let [height, setHeight] = useState("100vh");
   useEffect(() => {
     setHeight(`${window.innerHeight}px`);
@@ -42,34 +46,59 @@ function Padding({}) {
     };
   }, []);
 
+  return height;
+};
+
+function Padding({}) {
+  const height = useHeight();
+
   return (
     <>
       <div className="w-full select-none" style={{ height: height }}>
-        <div className="flex items-end justify-end h-full w-full">
+        <div className="flex items-center justify-end h-full w-full">
           <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Are you sleepy?`}</div>
         </div>
       </div>
       <div className="w-full select-none" style={{ height: height }}>
-        <div className="flex items-end justify-end h-full w-full">
+        <div className="flex items-center justify-end h-full w-full">
           <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Time to sleep.`}</div>
         </div>
       </div>
       <div className="w-full select-none" style={{ height: height }}>
-        <div className="flex items-end justify-end h-full w-full">
-          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`A new day`}</div>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Count the sheep.`}</div>
         </div>
       </div>
       <div className="w-full select-none" style={{ height: height }}>
-        <div className="flex items-end justify-end h-full w-full">
-          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Stress Body`}</div>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Good morning!`}</div>
         </div>
       </div>
       <div className="w-full select-none" style={{ height: height }}>
-        <div className="flex items-end justify-end h-full w-full">
-          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Magic Cloth Changing`}</div>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Let's get off bed.`}</div>
         </div>
       </div>
-      <div className="w-full select-none" style={{ height: height }}></div>
+      <div className="w-full select-none" style={{ height: height }}>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`A new day!`}</div>
+        </div>
+      </div>
+      <div className="w-full select-none" style={{ height: height }}>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Shine!`}</div>
+        </div>
+      </div>
+      <div className="w-full select-none" style={{ height: height }}>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`Let's go!`}</div>
+        </div>
+      </div>
+      <div className="w-full select-none" style={{ height: height }}>
+        <div className="flex items-center justify-end h-full w-full">
+          <div className="m-5 p-5 backdrop-blur-md rounded-2xl bg-white/30 text-6xl">{`...to sleep again. 😆`}</div>
+        </div>
+      </div>
     </>
   );
 }
